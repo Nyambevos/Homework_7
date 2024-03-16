2f075af9d06a1969c1534808b7a530eaff7db670
Nyambevos <ruslan.bilokoniuk@gmail.com>
2024-03-16 20:39:54 +0200

    Added connector for db and models

---
+++ models.py
@@ -0,0 +1,44 @@
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import DateTime
+
+
+Base = declarative_base()
+
+# Таблиця груп
+class StudentsGroups(Base):
+    __tablename__ = "students_group"
+    id = Column(Integer, primary_key=True)
+    student_group = Column(Integer, nullable=False)
+
+# Таблиця студентів
+class Students(Base):
+    __tablename__ = "students"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    group_id = Column(Integer, ForeignKey('students_group.id'))
+
+# Таблиця викладачів
+class Teachers(Base):
+    __tablename__ = "teachers"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+
+# Таблиця предметів
+class Subjects(Base):
+    __tablename__ = "subjects"
+    id = Column(Integer, primary_key=True)
+    subject_name = Column(String, nullable=False)
+    teacher_id = Column(Integer, ForeignKey('teachers.id'))
+
+# Таблиця оцінок
+class Grades(Base):
+    __tablename__ = "grades"
+    id = Column(Integer, primary_key=True)
+    student_id = Column(Integer, ForeignKey('students.id'))
+    subject_id = Column(Integer, ForeignKey('subjects.id'))
+    date_of = Column(DateTime, default=datetime.now())
+
